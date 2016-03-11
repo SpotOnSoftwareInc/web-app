7df47150ec7e2374e3da0b5a01afea24f2cb718b
Kurt Lang <kurtalang@gmail.com>
2016-03-10 16:33:53 -0800

    Make the visitor page look better

--- routes/webapp/staff/visitor.js
+++ routes/webapp/staff/visitor.js
@@ -16,59 +16,13 @@
 //};
 var async = require('async');
 
-//exports.get = function(req,res){
-//    console.log('Get function displayVisitorQueue');
-//    var database =  req.db;
-//    var visitorDB = database.get('visitor');
-//    var visitor;
-//    var employeeDB = database.get('employees');
-//
-//    var businessID = req.user[0].business.toString();
-//
-//    async.parallel({
-//            employee: function(cb){
-//                employeeDB.find({registrationToken: {$exists: false}, business: (businessID)},function (err,results){
-//
-//                    if (err) { return next(err);  }
-//                    if(!results) { return next(new Error('Error finding employee'));}
-//
-//                    employeee = results;
-//                    console.log(employeee);
-//                    cb();
-//
-//                });
-//            },
-//            nonemployee: function(cb){
-//                employeeDB.find({registrationToken: {$exists: true}, business: (businessID)}, function (err,results){
-//
-//
-//                    if (err) { return next(err); }
-//                    if(!results) { return next(new Error('Error finding employee'));}
-//
-//                    notemployee = results;
-//                    cb();
-//                });
-//            }
-//        },
-//        function(err,results){
-//            if(err){
-//                throw err;
-//            }
-//            res.render('staff/visitor',
-//                {
-//                    title: 'Visitor Queue',
-//                    notsigned: notemployee,
-//                    signed: employeee
-//                }
-//            )}
-//    )};
 
 exports.get = function (req, res) {
     console.log('Get function VisitorQueue');
     var database = req.db;
     var apptDB = database.get('appointment');
 
-    var bid = req.user[0].business;
+    var bid = req.user[0].business.toString();
 
     apptDB.find( { business: bid }, {state: 'waiting'} )
         .on('success', function(appointments) {
@@ -86,7 +40,7 @@ exports.get = function (req, res) {
 exports.post = function (req, res) {
 
     var appointmentDB = req.db.get('appointment');
-    var businessID = req.user[0].business.toString();
+    var bid = req.user[0].business.toString();
 
     var time = req.body.time;
     var provider = req.body.provider;
@@ -95,14 +49,20 @@ exports.post = function (req, res) {
     appointmentDB.insert({
         privider: provider,
         time: time,
-        name: name,
-        business: businessID
+        visitor: name,
+        business: bid,
+        state: 'waiting'
     });
 
-    res.render('staff/visitor',
-        {
-            provider: 'Kurt'
-        }
-    )
+    appointmentDB.find( { business: bid }, {state: 'waiting'} )
+        .on('success', function(appointments) {
+
+            res.render('staff/visitor', {
+                appts: appointments,
+
+                message: req.flash("Fetched all appointments")
+            });
+
+        })
 
 };
