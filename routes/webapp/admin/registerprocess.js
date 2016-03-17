507b86d0244d0d037f48b270f1d5a0bd2b67fea8
Julie Phakoom <sphakoom@ucsd.edu>
2016-03-16 17:23:15 -0700

    Added post method

--- routes/webapp/admin/registerprocess.js
+++ routes/webapp/admin/registerprocess.js
@@ -32,29 +32,47 @@ exports.post = function (req, res) {
     var companyAddress = req.body.companyAddress;
     var phone = req.body.phone;
 
+    var callingFunc = req.body.callingFunc;
+    var planName = req.body.planName;
+
     var businessDB = req.db.get('businesses');
     var bid = req.user[0].business;
 
-    console.log('Inside register process post method');
+    console.log('**Inside register process post method');
     console.log(businessDB);
 
-    businessDB.findAndModify({
-        query: {_id: bid},
-        update: {
-            $set: {
-                companyName: companyName,
-                companyAddress: companyAddress,
-                phone: phone
-        }
-    }});
+    /* User selecting payment plan */
+    if( callingFunc == 'updatePlan') {
+        console.log('**Updating plan');
+        businessDB.findAndModify({
+            query: {_id: bid},
+            update: {
+                $set: {
+                    plan: planName
+                }
+            }
+        });
+    }
+    else {
 
-    //res.render('admin/registerprocess', {
-    //    companyName: companyName,
-    //    companyAddress: companyAddress,
-    //    phone: phone
-    //});
-    res.redirect('/registerprocess');
+        businessDB.findAndModify({
+            query: {_id: bid},
+            update: {
+                $set: {
+                    companyName: companyName,
+                    companyAddress: companyAddress,
+                    phone: phone
+                }
+            }
+        });
 
+        //res.render('admin/registerprocess', {
+        //    companyName: companyName,
+        //    companyAddress: companyAddress,
+        //    phone: phone
+        //});
+        res.redirect('/registerprocess');
+    }
     //res.end(fname + companyName + email + password + username);
 
 };
