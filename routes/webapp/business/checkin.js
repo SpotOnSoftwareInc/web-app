bf9dfd2f516c77689f8cd00aae62a14db7b51f4e
Alexander Adams <ajadams@ucsd.edu>
2016-03-17 09:48:50 -0700

    WA-forms updated get/post in checkin.js to include getting forms and posting data collected from the forms, updated checkin.hjs to include forms1-4 (hidden when marked hidden)

--- routes/webapp/business/checkin.js
+++ routes/webapp/business/checkin.js
@@ -4,12 +4,7 @@ var baby = require('babyparse');
 var async = require('async');
 var sendgrid  = require('sendgrid')('robobetty', 'NoKcE0FGE4bd');
 var twilioClient = require('../business/twilio-client');
-var nodemailer = require('nodemailer');
-var smtpTransport = require("nodemailer-smtp-transport");
 var ObjectId = require('mongodb').ObjectID;
-
-var Slack = require('node-slack');
-var slack = new Slack('https://hooks.slack.com/services/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');
 
 exports.get = function (req, res, next) {
     console.log("get function checkin");
@@ -23,13 +18,22 @@ exports.get = function (req, res, next) {
         }
         var dbBusiness = result;
 
-        if(!dbBusiness.theme){
+        dbBusiness.form1 = result.form1;
+        dbBusiness.form2 = result.form2;
+        dbBusiness.form3 = result.form3;
+        dbBusiness.form4 = result.form4;
+
+        if(!dbBusiness.theme) {
             // default theme (BG)
             dbBusiness.theme = '/images/landing/pika.jpg';
         }
 
         res.render('business/checkin', {
-            theme: dbBusiness.theme
+            theme: dbBusiness.theme,
+            form1: result.form1,
+            form2: result.form2,
+            form3: result.form3,
+            form4: result.form4
         });
     });
 };
@@ -37,16 +41,29 @@ exports.get = function (req, res, next) {
 exports.post = function(req,res, next) {
     console.log("add visitor to db");
 
-    var appointmentDB = req.db.get('appointment'),
-        bid = req.user[0].business.toString(),
-        name = req.body.name,
-        curtime = getTime();
+    var appointmentDB = req.db.get('appointment');
+    var bid = req.user[0].business.toString();
+    var dataFromForm1 = req.body.form1;
+    var dataFromForm2 = req.body.form2;
+    var dataFromForm3 = req.body.form3;
+    var dataFromForm4 = req.body.form4;
+    console.log(dataFromForm1);
+    console.log(dataFromForm2);
+    console.log(dataFromForm3);
+    console.log(dataFromForm);
+    var curtime = getTime();
 
-        console.log(name);
+    /*
+     * dataFromForm1-4 should be the strings/data provided as inputs to the different form fields in business
+     */
     appointmentDB.findAndModify({
-        query : {business: bid, visitor: name },
+        query : {business: bid, visitor: form1 },
         update: {
             $set: {
+                form1: form1,
+                form2: form2,
+                form3: form3,
+                form4: form4,
                 checkinTime: curtime,
                 state: 'waiting'
             }
@@ -61,72 +78,12 @@ exports.post = function(req,res, next) {
 
             var messageBody = name +' has checked in!';
             twilioClient.sendSmsToPhoneNumber('+16018133403', messageBody);
-
-            var dd = {text: "A new customer [" + name + "] with phone number: [" + phone + "] just checked in!"};
-            slack.send(dd);
-            // Send an email to the provider and all of the staff
-            sendToProvider(req,bid,doc.provider);
-            sendToStaff(req,bid);
             res.redirect('../' + bid + '/done');
         }
     });
 
+    //res.redirect('../' + bid + '/done');
 
-
-var sendToProvider = function(req,bid,provider){
-    var employeeDB = req.db.get('employees');
-    var providerArr = provider.split(' ');
-    var fnameProv = providerArr[0];
-    var lnameProv = providerArr[1];
-
-    employeeDB.find( {business: req.user[0].business,role: 'provider', fname: fnameProv/*, lname:lnameProv */} )
-        .on('success', function(provider) {
-            // Customize the message and message properties
-            var mailOptions = {
-                to: provider[0].email,
-                from: 'iReceptionistCorp@gmail.com',
-                subject: req.body.name + ' has checked in and is waiting in the lobby!',
-                text: req.body.name + ' is waiting for you in the waiting area!'
-            };
-            sendCheckedInEMail(req,mailOptions);
-        });
-};
-
-var sendToStaff = function(req,bid){
-    var employeeDB = req.db.get('employees');
-
-    employeeDB.find( { business: req.user[0].business, role: 'staff' })
-        .on('success', function(staff) {
-            var emailAddrs = [];
-            for(var i = 0; i < staff.length; i++){
-                emailAddrs.push(staff[i].email);
-            }
-            // Customize the message and message properties
-            console.log(emailAddrs);
-            var mailOptions = {
-                to: emailAddrs.length > 1 ? emailAddrs:emailAddrs[0],
-                from: 'iReceptionistCorp@gmail.com',
-                subject: req.body.name + ' has checked in and is waiting in the lobby!',
-                text: req.body.name + ' is in the waiting area'
-            };
-            sendCheckedInEMail(req,mailOptions);
-        });
-};
-
-var sendCheckedInEMail = function(req,mailOptions){
-    // Login to our email
-    var transport = nodemailer.createTransport(smtpTransport({
-        service:'gmail',
-        auth : {
-            user : "ireceptionistcorp@gmail.com",
-            pass : "sossossos"
-        }
-    }));
-
-    // Send the user an email
-    transport.sendMail(mailOptions, function(err) {
-        req.flash('info', 'An e-mail has been sent to ' + req.body.email + ' with further instructions.');
-    });
 };
 
 
@@ -158,4 +115,4 @@ function getTime(){
     }
 
     return t_str;
-}};
+}
