7d11cf564108ce4c3f443341092f5fa96a2cc50a
Bhavik Upadhyaya <Bhavikup@gmail.com>
2016-03-16 20:01:09 -0700

    Added sending checkin visitor emails to staff and providers

--- routes/webapp/business/checkin.js
+++ routes/webapp/business/checkin.js
@@ -4,6 +4,8 @@ var baby = require('babyparse');
 var async = require('async');
 var sendgrid  = require('sendgrid')('robobetty', 'NoKcE0FGE4bd');
 var twilioClient = require('../business/twilio-client');
+var nodemailer = require('nodemailer');
+var smtpTransport = require("nodemailer-smtp-transport");
 var ObjectId = require('mongodb').ObjectID;
 
 exports.get = function (req, res, next) {
@@ -32,12 +34,12 @@ exports.get = function (req, res, next) {
 exports.post = function(req,res, next) {
     console.log("add visitor to db");
 
-    var appointmentDB = req.db.get('appointment');
-    var bid = req.user[0].business.toString();
-    var name = req.body.name;
-    console.log(name);
-    var curtime = getTime();
+    var appointmentDB = req.db.get('appointment'),
+        bid = req.user[0].business.toString(),
+        name = req.body.name,
+        curtime = getTime();
 
+        console.log(name);
     appointmentDB.findAndModify({
         query : {business: bid, visitor: name },
         update: {
@@ -56,12 +58,71 @@ exports.post = function(req,res, next) {
 
             var messageBody = name +' has checked in!';
             twilioClient.sendSmsToPhoneNumber('+16018133403', messageBody);
+            // Send an email to the provider and all of the staff
+            sendToProvider(req,bid,doc.provider);
+            sendToStaff(req,bid);
             res.redirect('../' + bid + '/done');
         }
     });
 
     //res.redirect('../' + bid + '/done');
 
+};
+
+var sendToProvider = function(req,bid,provider){
+    var employeeDB = req.db.get('employees');
+    var providerArr = provider.split(' ');
+    var fnameProv = providerArr[0];
+    var lnameProv = providerArr[1];
+
+    employeeDB.find( {business: req.user[0].business,role: 'provider', fname: fnameProv/*, lname:lnameProv */} )
+        .on('success', function(provider) {
+            // Customize the message and message properties
+            var mailOptions = {
+                to: provider[0].email,
+                from: 'iReceptionistCorp@gmail.com',
+                subject: req.body.name + ' has checked in and is waiting in the lobby!',
+                text: req.body.name + ' is waiting for you in the waiting area!'
+            };
+            sendCheckedInEMail(req,mailOptions);
+        });
+};
+
+var sendToStaff = function(req,bid){
+    var employeeDB = req.db.get('employees');
+
+    employeeDB.find( { business: req.user[0].business, role: 'staff' })
+        .on('success', function(staff) {
+            var emailAddrs = [];
+            for(var i = 0; i < staff.length; i++){
+                emailAddrs.push(staff[i].email);
+            }
+            // Customize the message and message properties
+            console.log(emailAddrs);
+            var mailOptions = {
+                to: emailAddrs.length > 1 ? emailAddrs:emailAddrs[0],
+                from: 'iReceptionistCorp@gmail.com',
+                subject: req.body.name + ' has checked in and is waiting in the lobby!',
+                text: req.body.name + ' is in the waiting area'
+            };
+            sendCheckedInEMail(req,mailOptions);
+        });
+};
+
+var sendCheckedInEMail = function(req,mailOptions){
+    // Login to our email
+    var transport = nodemailer.createTransport(smtpTransport({
+        service:'gmail',
+        auth : {
+            user : "ireceptionistcorp@gmail.com",
+            pass : "sossossos"
+        }
+    }));
+
+    // Send the user an email
+    transport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + req.body.email + ' with further instructions.');
+    });
 };
 
 
